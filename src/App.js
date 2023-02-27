6d3e227c2e21038ec6927608e01b46209e6798a9
agresh0 <agresh.prof@gmail.com>
2023-02-27 21:08:08 +0530

    v1 27-02-2023

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route,Routes } from 'react-router-dom';
 import './App.css';
+import LoginPage from './component/authentication/LoginPage';
+import SignUpPage from './component/authentication/SignUpPage';
+import Home from './component/Dashboard/Home';
 
 function App() {
   return (
     <div className="App">
+     <Routes>
+     <Route path='/' element={<LoginPage/>}/>
+     {/* SignUpPage Route */}
+     <Route path='/register' element={<SignUpPage/>}/>
+     <Route path='/home' element={<Home/>}/>
+     </Routes>
+     
     </div>
   );
 }
--- src/component/SignPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SignPage() {
-  return (
-    <div>SignPage</div>
-  )
-}
-
-export default SignPage
